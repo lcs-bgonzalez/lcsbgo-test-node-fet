9bdb0085fdf8dffb2f8feb1be41f98f68a6506e4
Borja González <bgonzalez@leadclic.com>
2017-03-22 19:03:54 +0100

    Initial

---
+++ server.js
@@ -0,0 +1,12 @@
+﻿var rest = require('express')();
+var httpServer = require('http').Server(rest);
+
+const PORT = process.env.PORT || 3000;
+
+httpServer.listen(PORT, function(){
+	console.log(`Listening on ${ PORT }`);
+});
+
+rest.get('/:variable', function (req, res) {
+	res.send('despedido ' + req.params.variable)
+});
