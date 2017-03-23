02216af8003eab627535b3bf19ac3e6f13ac5da9
Borja González <bgonzalez@leadclic.com>
2017-03-23 17:59:50 +0100

    test

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ pg.defaults.ssl = true;
 pg.connect(process.env.DATABASE_URL || "postgres://ub3sn6j7hnsapl:p6ac34ea3e5868be0f78e8b2341053f1b672694973ddfc4eaaed7966b56a82df4@ec2-54-247-107-132.eu-west-1.compute.amazonaws.com:5432/d9atqk42arg1jd?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
-
+  testVar = "";
   client
     .query('SELECT table_schema,table_name FROM information_schema.tables;')
     .on('row', function(row) {
       console.log(JSON.stringify(row));
-	  testVar = JSON.stringify(row);
+	  testVar += JSON.stringify(row);
     });
 });
 
